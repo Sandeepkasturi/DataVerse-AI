7b62408527920f15691e1bd638f617b48b65bdf0
Sandeep Kasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2023-12-13 00:19:53 +0530

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -386,11 +386,11 @@ export const ChatInput = ({
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          DataVerse AI
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality. Proudly Developed by SKAV",
         )}
       </div>
     </div>
